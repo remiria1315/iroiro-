7dde27f79e1fbf588ad678a571cbe5fa3ad80a4e
remiria1315 <remiria1315@outlook.jp>
2025-08-28 13:39:36 +0900

    uploaddddddddddddddddd

---
+++ index.js
@@ -0,0 +1,31 @@
+﻿const { app, BrowserWindow, screen } = require("electron");
+
+app.whenReady().then(() => {
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize;
+
+  const winWidth = 512;
+  const winHeight = 512;
+
+  const win = new BrowserWindow({
+    width: winWidth,
+    height: winHeight,
+    x: screenWidth - winWidth,
+    y: 0,
+    alwaysOnTop: true,
+    frame: false,
+    transparent: true,
+    resizable: false,
+    movable: false,
+    webPreferences: { nodeIntegration: true },
+  });
+
+  win.loadFile("index.html");
+  win.setIgnoreMouseEvents(true, { forward: true });
+  win.setBounds({
+    x: screenWidth - winWidth,
+    y: 0,
+    width: winWidth,
+    height: winHeight,
+  });
+});
